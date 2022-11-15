e2c6af08531fb3c7ea9613b3b5669acb7ea9bae7
Renzo Beccari <90480590+Ren991@users.noreply.github.com>
2022-11-15 10:14:14 -0300

    primeros cambios

---
+++ src/App.js
@@ -0,0 +1,76 @@
+import React, { useState, useEffect } from 'react';
+import { commerce } from './lib/commerce'
+//import Products from './components/Products/Products'
+//import Navbar from './components/Navbar/Navbar';
+import { Products, Navbar, Cart,Checkout } from './components';
+import { BrowserRouter ,Switch, Route, Navigate } from 'react-router-dom';
+
+const App = () => {
+    const [products, setProducts] = useState([]);
+    const [cart, setCart] = useState({});
+
+    const fetchProducts = async () => {
+        const { data } = await commerce.products.list();
+        setProducts(data);
+    }
+    const fetchCart = async () => {
+
+        setCart(await commerce.cart.retrieve())
+    }
+    const handleAddToCart = async (productId, quantity) => {
+
+        setCart(await commerce.cart.add(productId, quantity));
+    }
+    const handleUpdateCartQty =async(productId,quantity)=>{
+        setCart(await commerce.cart.update(productId,{quantity}));
+        
+    }
+
+    const handleRemoveFromCart= async(productId)=>{
+        
+        setCart(await commerce.cart.remove(productId))
+    }
+    
+    const handleEmptyCart = async ()=>{
+         
+        setCart(await commerce.cart.empty())
+    }
+
+    useEffect(() => { // component did mount
+        fetchProducts();
+        fetchCart()
+    }, [])
+    console.log(products)
+    console.log(cart)
+    return (
+
+        <BrowserRouter>
+            <div>
+            <Navbar totalItems={cart?.total_items} />
+                <Switch>
+                   
+                    <Route exact path="/">
+                          <Products products={products} onAddToCart={handleAddToCart}/> 
+                    </Route>
+                    <Route exact path="/cart">
+                        <Cart cart={cart} 
+                        handleUpdateCartQty={handleUpdateCartQty}
+                        handleRemoveFromCart={handleRemoveFromCart}
+                        handleEmptyCart={handleEmptyCart}
+                        />
+                    </Route>
+                    <Route exact path="/checkout">
+                        <Checkout cart={cart} />
+
+                    </Route>
+                    
+                   
+                    
+                </Switch>
+            </div>
+        </BrowserRouter>
+
+
+    )
+}
+export default App
---
+++ src/lib/commerce.js
@@ -0,0 +1,3 @@
+import Commerce from '@chec/commerce.js';
+
+export const commerce = new Commerce("pk_test_482973edfa2bcd3a586e9ab12b7bb8102dd9a63f18048",true);
